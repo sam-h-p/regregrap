df30b551712e2fccd863edb0cdfa6d98f613c62e
Sam Henderson-Palmer <samhendersonpalmer@gmail.com>
2023-06-20 15:11:52 +0100

    Adding RDA files

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' GOV.UK register register.
+#'
+#' A dataset containing the characteristics of all
+#'  the published GOV.UK registers.
+#'
+#'  format: A data frame
+#'  \describe{
+#'    \item{register}{name of register}
+#'    \item{text}{description of register contents}
+#'    \item{registry}{responsible body of register}
+#'    \item{phase}{The stafe of development}
+#'    \item{copyright}{The copyright terms}
+#'    ...
+#'  }
+#'    source: \url{https://webarchive.nationalarchives.gov.uk/ukgwa/20210104111014/https://www.registers.service.gov.uk/registers/register/download}
+"regreg"
--- data-raw/create_regreg.R
+++ data-raw/create_regreg.R
@@ -1,5 +1,12 @@
+# some code to go from raw data csv to a nice RDA object
+
+# create_regreg
+
 regreg <- read.csv("./data-raw/register.csv", check.names = TRUE)
 
-usethis::use_data(regreg)
+regreg$phase <- as.factor(regreg$phase)
+
+usethis::use_data(regreg, overwrite = TRUE)
 
 rm(regreg)
+
